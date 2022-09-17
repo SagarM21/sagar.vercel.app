64fab52f352ad59acdda1cf3127d58f77519312c
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-18 00:48:07 +0530

    scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
@@ -9,7 +10,7 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
 	return (
-		<div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+		<div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0'>
 			<Head>
 				<title>Sagar's Portfolio</title>
 			</Head>
@@ -35,6 +36,10 @@ const Home: NextPage = () => {
 
 			<section id='projects' className='snap-start'>
 				<Projects />
+			</section>
+
+			<section id='contact' className='snap-start'>
+				<ContactMe />
 			</section>
 		</div>
 	);
