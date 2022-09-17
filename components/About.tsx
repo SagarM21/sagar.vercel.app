39d8e8b89481b6bdf8888d3c382dc1f4ec381e8e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-17 23:12:29 +0530

    About update

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,12 @@ type Props = {};
 
 function About({}: Props) {
 	return (
-		<div className='flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center h-screen'>
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+			className='flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center h-screen'
+		>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
 				About
 			</h3>
@@ -18,7 +23,18 @@ function About({}: Props) {
 				src=''
 				className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
 			/>
-		</div>
+
+			<div className='space-y-10 px-0 md:px-10'>
+				<h4 className='text-4xl font-semibold'>
+					Here is a{" "}
+					<span className='underline decoration-[#F7AB0A]/50'>little</span>{" "}
+					background
+				</h4>
+				<p className='text-base'>
+					I'm Sagar, a Full Stack Developer who loves to code sometimes!
+				</p>
+			</div>
+		</motion.div>
 	);
 }
 
