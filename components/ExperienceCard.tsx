0fda3d2970d954d004d7bca19fd2c5954f0c4780
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-17 23:27:24 +0530

    experience 1/4

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const ExperienceCard = (props: Props) => {
-	return <div>ExperienceCard</div>;
+	return (
+		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+			<motion.img
+				initial={{
+					y: -100,
+					opacity: 0,
+				}}
+				transition={{ duration: 1.2 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				viewport={{ once: true }}
+				className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+				src=''
+			/>
+
+			<div className='px-0 md:px-10'>
+				<h4 className='text-4xl font-light'>Frontend Developer Intern</h4>
+				<p className='font-bold mt-1 text-2xl'>Doosra College</p>
+				<div className='flex space-x-2 my-2'>
+					{/* Tech used */}
+					<img className='h-10 w-10 rounded-full' src='' alt='' />
+				</div>
+				<p className='uppercase py-5 text-gray-300'>Started work...</p>
+
+				<ul className='list-disc space-y-4 ml-5 text-lg'>
+					<li>Text</li>
+					<li>Text</li>
+					<li>Text</li>
+					<li>Text</li>
+				</ul>
+			</div>
+		</article>
+	);
 };
 
 export default ExperienceCard;
