8ad8927af29ad1980d260b2b7d97fff0bc0a154d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-17 23:12:40 +0530

    Experience structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
 	return (
@@ -21,6 +22,10 @@ const Home: NextPage = () => {
 			<section id='about' className='snap-center'>
 				<About />
 			</section>
+
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 		</div>
 	);
 };
