811424f676cf5095633d7c78a957258daec254e6
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-17 22:06:52 +0530

    framer effects

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
 	return (
 		<header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl  mx-auto z-20 xl:items-center '>
-			<div className='flex flex-row items-center'>
+			<motion.div
+				initial={{ x: -500, opacity: 0, scale: 0.5 }}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className='flex flex-row items-center'
+			>
 				<SocialIcon
 					url='https://twitter.com/Sagar_Maheshh'
 					fgColor='grey'
@@ -22,8 +34,18 @@ function Header({}: Props) {
 					fgColor='grey'
 					bgColor='transparent'
 				/>
-			</div>
-			<div className='flex items-center text-gray-300 cursor-pointer'>
+			</motion.div>
+
+			<motion.div
+				initial={{ x: 500, opacity: 0, scale: 0.5 }}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{ duration: 1.5 }}
+				className='flex items-center text-gray-300 cursor-pointer'
+			>
 				<SocialIcon
 					className='cursor-pointer'
 					network='email'
@@ -33,7 +55,7 @@ function Header({}: Props) {
 				<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
 					Get In Touch
 				</p>
-			</div>
+			</motion.div>
 		</header>
 	);
 }
