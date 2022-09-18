c56565d8661783f016ba8aa8bd337f6d864ac899
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-18 14:37:58 +0530

    fetch experience from sanity

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Experience } from "../typings";
+import { urlFor } from "../sanity";
 
-type Props = {};
+type Props = {
+	experience: Experience;
+};
 
-const ExperienceCard = (props: Props) => {
+const ExperienceCard = ({ experience }: Props) => {
 	return (
 		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
 			<motion.img
@@ -15,23 +19,32 @@ const ExperienceCard = (props: Props) => {
 				whileInView={{ opacity: 1, y: 0 }}
 				viewport={{ once: true }}
 				className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
-				src=''
+				src={urlFor(experience?.companyImage).url()}
 			/>
 
 			<div className='px-0 md:px-10'>
-				<h4 className='text-4xl font-light'>Frontend Developer Intern</h4>
-				<p className='font-bold mt-1 text-2xl'>Doosra College</p>
+				<h4 className='text-4xl font-light'>{experience?.jobTitle}</h4>
+				<p className='font-bold mt-1 text-2xl'>{experience?.company}</p>
 				<div className='flex space-x-2 my-2'>
-					{/* Tech used */}
-					<img className='h-10 w-10 rounded-full' src='' alt='' />
+					{experience.technologies.map((technology) => (
+						<img
+							key={technology._id}
+							src={urlFor(technology.image).url()}
+							className='h-10 w-10 rounded-full'
+						/>
+					))}
 				</div>
-				<p className='uppercase py-5 text-gray-300'>Started work...</p>
+				<p className='uppercase py-5 text-gray-300'>
+					{new Date(experience.dateStarted).toDateString()} -{" "}
+					{experience.isCurrentlyWorkingHere
+						? "Present"
+						: new Date(experience.dateEnded).toDateString()}
+				</p>
 
-				<ul className='list-disc space-y-4 ml-5 text-lg'>
-					<li>Text</li>
-					<li>Text</li>
-					<li>Text</li>
-					<li>Text</li>
+				<ul className='list-disc space-y-4 ml-5 text-lg max-h-96 pr-5 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80'>
+					{experience.points.map((point, i) => (
+						<li key={i}>{point}</li>
+					))}
 				</ul>
 			</div>
 		</article>
