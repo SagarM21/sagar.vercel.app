e5f466427f7a30deea3dc69e28ce2d191b98f980
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-18 14:48:27 +0530

    fetch skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Skill } from "../typings";
+import { urlFor } from "../sanity";
 
 type Props = {
 	directionLeft?: boolean;
+	skill: Skill;
 };
 
-const Skill = ({ directionLeft }: Props) => {
+const Skill = ({ directionLeft, skill }: Props) => {
 	return (
 		<div className='group relative flex cursor-pointer'>
 			<motion.img
 				initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
-				className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+				className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28  filter group-hover:grayscale transition duration-300 ease-in-out'
+				src={urlFor(skill?.image).url()}
 			/>
-			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 rounded-full z-0'>
 				<div className='flex items-center justify-center h-full'>
-					<p className='text-3xl font-bold opacity-100 text-black'>100%</p>
+					<p className='text-3xl font-bold opacity-100 text-black'>
+						{skill?.progress}%
+					</p>
 				</div>
 			</div>
 		</div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Skill as SkillType } from "../typings";
 import Skill from "./Skill";
 
-type Props = {};
+type Props = {
+	skills: SkillType[];
+};
 
-const Skills = (props: Props) => {
+const Skills = ({ skills }: Props) => {
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -21,17 +24,13 @@ const Skills = (props: Props) => {
 			</h3>
 
 			<div className='grid grid-cols-4 gap-5 '>
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
-				<Skill />
+				{skills?.slice(0, skills.length / 2).map((skill) => (
+					<Skill key={skill._id} skill={skill} />
+				))}
+
+				{skills?.slice(skills.length / 2, skills.length).map((skill) => (
+					<Skill key={skill._id} skill={skill} directionLeft />
+				))}
 			</div>
 		</motion.div>
 	);
