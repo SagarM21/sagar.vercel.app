a3e0e4a5225f82374da349886554c1dbf0cf7fca
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-18 13:10:48 +0530

    hero page done fetching

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { urlFor } from "../sanity";
+import { PageInfo } from "../typings";
 import BackgroundCircles from "./BackgroundCircles";
 
-type Props = {};
+type Props = {
+	pageInfo: PageInfo;
+};
 
-function Hero({}: Props) {
+function Hero({ pageInfo }: Props) {
 	const [text, count] = useTypewriter({
 		words: [
-			"Hi, The Name's Sagar Maheshwari",
+			`Hi, The Name's ${pageInfo?.name}`,
 			"Guy-who-loves-building.tsx",
 			"<ButLovesCoffeeMore />",
 		],
@@ -19,13 +23,13 @@ function Hero({}: Props) {
 		<div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
 			<BackgroundCircles />
 			<img
-				src=''
+				src={urlFor(pageInfo?.heroImage).url()}
 				alt=''
 				className='relative w-32 h-32 rounded-full mx-auto object-cover'
 			/>
-			<div className="z-20">
+			<div className='z-20'>
 				<h2 className='text-sm uppercase text-gray-500 tracking-[15px]'>
-					Full Stack Developer
+					{pageInfo?.role}
 				</h2>
 				<h1 className='text-5xl xl:text-6xl font-semibold px-10'>
 					<span className='mr-3'>{text}</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
-import { profile } from "../assets/sagar-profile1.jpg";
-import Image from "next/image";
 import { Experience, PageInfo, Project, Skill, Social } from "../typings";
 import { fetchPageInfo } from "../utils/fetchPageInfo";
 import { fetchExperiences } from "../utils/fetchExperiences";
@@ -35,7 +33,7 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
 
 			{/* Hero */}
 			<section id='hero' className='snap-start'>
-				<Hero />
+				<Hero pageInfo={pageInfo} />
 			</section>
 
 			{/* About */}
@@ -64,7 +62,7 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
 					<div className='flex items-center justify-center'>
 						<img
 							className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
-							src={profile}
+							src=''
 							alt=''
 						/>
 					</div>
