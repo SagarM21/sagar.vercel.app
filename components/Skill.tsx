f4d56e83300a5762fa744f80414a13fecbb6ddea
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-21 12:18:54 +0530

    rectifying skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -15,10 +15,10 @@ const Skill = ({ directionLeft, skill }: Props) => {
 				initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
-				className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28  filter group-hover:grayscale transition duration-300 ease-in-out'
+				className='rounded-full border border-gray-500 object-cover w-20 h-20 md:w-24 md:h-24 filter group-hover:grayscale transition duration-300 ease-in-out'
 				src={urlFor(skill?.image).url()}
 			/>
-			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 rounded-full z-0'>
+			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 md:w-24 md:h-24 ease-in-out group-hover:bg-white h-20 w-20 rounded-full z-0'>
 				<div className='flex items-center justify-center h-full'>
 					<p className='text-3xl font-bold opacity-100 text-black'>
 						{skill?.progress}%
